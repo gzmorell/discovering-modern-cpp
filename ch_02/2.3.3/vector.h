1f2daf332fcc834c59bc7d1e73364f2bef2638fb
Francisco Gonzalez <gzmorell@gmail.com>
2020-06-17 23:33:54 +0200

    Added const operator, some corrections and reformat

--- ch_02/2.3.3/vector.h
+++ ch_02/2.3.3/vector.h
@@ -3,72 +3,94 @@
 #include <initializer_list>
 #include <iostream>
 
-namespace curso {
+namespace curso
+{
 
-class vector {
-  int _size;
-  double *data;
+  class vector
+  {
+    int _size;
+    double *data;
 
-public:
-  vector(int size = 0) : _size(size), data(nullptr) {
-    if (_size > 0)
-      data = new double[_size];
-    std::cout << "Vector created." << std::endl;
-  };
-  vector(const vector &other) : _size(other._size), data(new double[_size]) {
-    for (int i = 0; i < _size; ++i)
-      data[i] = other.data[i];
-    std::cout << "Vector copied." << std::endl;
-  };
-  vector(vector &&other) {
-    std::cout << "Vector moved." << std::endl;
-    data = other.data;
-    _size = other._size;
-    other._size = 0;
-    delete [] other.data;
-  }
+  public:
+    vector(int size = 0) : _size(size), data(nullptr)
+    {
+      if (_size > 0)
+        data = new double[_size];
+      else
+      {
+        _size = 0;
+      }
 
-  explicit vector(std::initializer_list<double> values)
-      : _size(values.size()), data(new double[_size]) {
-    std::copy(std::begin(values), std::end(values), data);
-  }
-  vector &operator=(const vector &other) {
-    assert(_size == other._size);
-    if (data == other.data)
+      std::cout << "Vector created." << std::endl;
+    };
+    vector(const vector &other) : _size(other._size), data(new double[_size])
+    {
+      for (int i = 0; i < _size; ++i)
+        data[i] = other.data[i];
+      std::cout << "Vector copied." << std::endl;
+    };
+    vector(vector &&other)
+    {
+      std::cout << "Vector moved." << std::endl;
+      data = other.data;
+      _size = other._size;
+      other._size = 0;
+      delete[] other.data;
+    }
+
+    explicit vector(std::initializer_list<double> values)
+        : _size(values.size()), data(new double[_size])
+    {
+      std::copy(std::begin(values), std::end(values), data);
+    }
+    vector &operator=(const vector &other)
+    {
+      assert(_size == other._size);
+      if (data == other.data)
+        return *this;
+      for (int i = 0; i < _size; ++i)
+        data[i] = other.data[i];
+      std::cout << "Vector assigned." << std::endl;
       return *this;
-    for (int i = 0; i < _size; ++i)
-      data[i] = other.data[i];
-    std::cout << "Vector assigned." << std::endl;
-    return *this;
-  }
-  vector &operator=(std::initializer_list<double> values) {
-    assert(_size = values.size());
-    std::copy(std::begin(values), std::end(values), data + _size);
-    return *this;
-  }
-  vector &operator=(vector&& other) {
-    if (this == &other)
+    }
+    vector &operator=(std::initializer_list<double> values)
+    {
+      assert(_size = values.size());
+      std::copy(std::begin(values), std::end(values), data + _size);
       return *this;
-    delete [] data;
-    data = other.data;
-    _size = other._size;
-    other.data = nullptr;
-    other._size = 0;
-    return *this;
-  }
-  ~vector() {
-    _size = 0;
-    delete[] data;
-    std::cout << "Vector destroyed." << std::endl;
-  }
-  int size() const { return _size; }
-  double at(int i) const {
-    assert(i < _size);
-    return data[i];
-  }
-  double &operator[](int i) {
-    assert(i < _size);
-    return data[i];
-  }
-};
+    }
+    vector &operator=(vector &&other)
+    {
+      if (this == &other)
+        return *this;
+      delete[] data;
+      data = other.data;
+      _size = other._size;
+      other.data = nullptr;
+      other._size = 0;
+      return *this;
+    }
+    ~vector()
+    {
+      _size = 0;
+      delete[] data;
+      std::cout << "Vector destroyed." << std::endl;
+    }
+    int size() const { return _size; }
+    double at(int i) const
+    {
+      assert(i >= 0 && i < _size);
+      return data[i];
+    }
+    double &operator[](int i)
+    {
+      assert(i >= 0 && i < _size);
+      return data[i];
+    }
+    const double &operator[](int i) const
+    {
+      assert(i >= 0 && i < _size);
+      return data[i];
+    }
+  };
 } // namespace curso
